018a5facb40fa876429457d09ed61a56b346553d
diana <diana@DESKTOP-UM4AAO6>
2021-12-02 15:53:30 +0100

    VChat arreglada

--- Practica5/src/VChat.java
+++ Practica5/src/VChat.java
@@ -30,6 +30,7 @@ public class VChat extends javax.swing.JDialog {
         this.padre = (VAutentificacion) padre;
         this.serv = serv;
         enviar.setEnabled(false);
+
     }
 
     public void setUsuario(Usuario usuario) {
@@ -54,20 +55,20 @@ public class VChat extends javax.swing.JDialog {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jPanel3 = new javax.swing.JPanel();
-        jScrollPane3 = new javax.swing.JScrollPane();
-        jMensajeEnviar = new javax.swing.JTextPane();
-        enviar = new javax.swing.JButton();
-        solicitudes = new javax.swing.JButton();
-        jLabel8 = new javax.swing.JLabel();
-        jChat = new java.awt.TextArea();
         jScrollPane4 = new javax.swing.JScrollPane();
         jTablaAmigos = new javax.swing.JTable();
         saludoCliente = new javax.swing.JLabel();
-        jLabel9 = new javax.swing.JLabel();
         jLabel10 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jMensajeEnviar = new javax.swing.JTextPane();
+        solicitudes = new javax.swing.JButton();
+        enviar = new javax.swing.JButton();
+        jchatear = new javax.swing.JLabel();
+        jChat = new java.awt.TextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(637, 300));
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowClosing(java.awt.event.WindowEvent evt) {
                 formWindowClosing(evt);
@@ -76,9 +77,24 @@ public class VChat extends javax.swing.JDialog {
 
         jPanel1.setBackground(new java.awt.Color(244, 249, 255));
         jPanel1.setFont(new java.awt.Font("OCR A Extended", 0, 16)); // NOI18N
+        jPanel1.setMaximumSize(new java.awt.Dimension(500, 500));
+        jPanel1.setMinimumSize(new java.awt.Dimension(300, 0));
 
-        jPanel3.setBackground(new java.awt.Color(244, 242, 255));
-        jPanel3.setFont(new java.awt.Font("OCR A Extended", 0, 16)); // NOI18N
+        jTablaAmigos.setFont(new java.awt.Font("OCR A Extended", 0, 18)); // NOI18N
+        jTablaAmigos.setModel(new ModeloTablaUsuarios());
+        jTablaAmigos.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jTablaAmigosMouseClicked(evt);
+            }
+        });
+        jScrollPane4.setViewportView(jTablaAmigos);
+
+        saludoCliente.setFont(new java.awt.Font("Segoe UI Black", 0, 30)); // NOI18N
+
+        jLabel10.setFont(new java.awt.Font("Segoe UI Black", 0, 30)); // NOI18N
+        jLabel10.setText("CONECTADOS:");
+
+        jPanel2.setBackground(new java.awt.Color(244, 242, 255));
 
         jMensajeEnviar.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyPressed(java.awt.event.KeyEvent evt) {
@@ -89,15 +105,6 @@ public class VChat extends javax.swing.JDialog {
             }
         });
         jScrollPane3.setViewportView(jMensajeEnviar);
-
-        enviar.setBackground(new java.awt.Color(244, 242, 255));
-        enviar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons8-enviar-mensaje-48.png"))); // NOI18N
-        enviar.setBorder(null);
-        enviar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                enviarActionPerformed(evt);
-            }
-        });
 
         solicitudes.setBackground(new java.awt.Color(252, 246, 220));
         solicitudes.setFont(new java.awt.Font("OCR A Extended", 0, 18)); // NOI18N
@@ -111,58 +118,50 @@ public class VChat extends javax.swing.JDialog {
             }
         });
 
-        jLabel8.setFont(new java.awt.Font("Segoe UI Black", 0, 30)); // NOI18N
-        jLabel8.setText("CHAT:");
+        enviar.setBackground(new java.awt.Color(244, 242, 255));
+        enviar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons8-enviar-mensaje-48.png"))); // NOI18N
+        enviar.setBorder(null);
+        enviar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                enviarActionPerformed(evt);
+            }
+        });
 
-        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
-        jPanel3.setLayout(jPanel3Layout);
-        jPanel3Layout.setHorizontalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jChat, javax.swing.GroupLayout.PREFERRED_SIZE, 331, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(jPanel3Layout.createSequentialGroup()
+        jchatear.setFont(new java.awt.Font("Segoe UI Black", 0, 30)); // NOI18N
+        jchatear.setText("CHAT:");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
                         .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 254, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
                         .addComponent(enviar, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel3Layout.createSequentialGroup()
-                        .addGap(164, 164, 164)
+                    .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addComponent(jchatear, javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jChat, javax.swing.GroupLayout.PREFERRED_SIZE, 331, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addComponent(solicitudes, javax.swing.GroupLayout.PREFERRED_SIZE, 167, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(90, 90, 90))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
-        jPanel3Layout.setVerticalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
                 .addComponent(solicitudes, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel8, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(6, 6, 6)
+                .addComponent(jchatear)
+                .addGap(23, 23, 23)
                 .addComponent(jChat, javax.swing.GroupLayout.PREFERRED_SIZE, 353, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(19, 19, 19)
-                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(enviar, javax.swing.GroupLayout.Alignment.TRAILING))
-                .addContainerGap())
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 49, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(enviar))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
-
-        jTablaAmigos.setFont(new java.awt.Font("OCR A Extended", 0, 18)); // NOI18N
-        jTablaAmigos.setModel(new ModeloTablaUsuarios());
-        jTablaAmigos.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jTablaAmigosMouseClicked(evt);
-            }
-        });
-        jScrollPane4.setViewportView(jTablaAmigos);
-
-        saludoCliente.setFont(new java.awt.Font("Segoe UI Black", 0, 30)); // NOI18N
-
-        jLabel9.setFont(new java.awt.Font("Segoe UI Black", 0, 30)); // NOI18N
-
-        jLabel10.setFont(new java.awt.Font("Segoe UI Black", 0, 30)); // NOI18N
-        jLabel10.setText("CONECTADOS:");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -171,31 +170,23 @@ public class VChat extends javax.swing.JDialog {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(saludoCliente, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGap(29, 29, 29)
-                        .addComponent(jLabel9, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGap(178, 178, 178))
-                    .addComponent(jLabel10, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 231, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(0, 0, Short.MAX_VALUE)))
-                .addGap(28, 28, 28)
-                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, 363, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 231, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel10, javax.swing.GroupLayout.PREFERRED_SIZE, 231, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(saludoCliente, javax.swing.GroupLayout.PREFERRED_SIZE, 250, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 36, Short.MAX_VALUE)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(saludoCliente, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel9, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(11, 11, 11)
-                .addComponent(jLabel10, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap()
+                .addComponent(saludoCliente, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel10, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
                 .addComponent(jScrollPane4, javax.swing.GroupLayout.PREFERRED_SIZE, 420, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
-            .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap(24, Short.MAX_VALUE))
+            .addComponent(jPanel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -206,7 +197,9 @@ public class VChat extends javax.swing.JDialog {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
@@ -214,9 +207,9 @@ public class VChat extends javax.swing.JDialog {
 
     private void enviarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_enviarActionPerformed
         // TODO add your handling code here:
-        jChat.append(usuario.getNombre()+":  "+jMensajeEnviar.getText()+"\n");
+        jChat.append(usuario.getNombre() + ":  " + jMensajeEnviar.getText() + "\n");
         try {
-            amigoChat.getInterfaz().recibirMensaje(usuario.getNombre()+":  "+jMensajeEnviar.getText()+"\n",usuario.getNombre());
+            amigoChat.getInterfaz().recibirMensaje(usuario.getNombre() + ":  " + jMensajeEnviar.getText() + "\n", usuario.getNombre());
         } catch (RemoteException ex) {
             Logger.getLogger(VChat.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -255,6 +248,7 @@ public class VChat extends javax.swing.JDialog {
         ModeloTablaUsuarios amigosUsuario;
         amigosUsuario = (ModeloTablaUsuarios) jTablaAmigos.getModel();
         this.amigoChat = usuario.getAmigos().get((String) amigosUsuario.getValueAt(jTablaAmigos.getSelectedRow(), 0));
+        jchatear.setText("Chatea con " + amigoChat.getNombre().toUpperCase());
         jChat.setText("");
 
     }//GEN-LAST:event_jTablaAmigosMouseClicked
@@ -265,7 +259,7 @@ public class VChat extends javax.swing.JDialog {
 
     private void jMensajeEnviarKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jMensajeEnviarKeyReleased
         // TODO add your handling code here:
-        if (jMensajeEnviar.getText().isEmpty() && this.amigoChat==null) {
+        if (jMensajeEnviar.getText().isEmpty() && this.amigoChat == null) {
             enviar.setEnabled(false);
         } else {
             enviar.setEnabled(true);
@@ -297,14 +291,13 @@ public class VChat extends javax.swing.JDialog {
     private javax.swing.JButton enviar;
     private java.awt.TextArea jChat;
     private javax.swing.JLabel jLabel10;
-    private javax.swing.JLabel jLabel8;
-    private javax.swing.JLabel jLabel9;
     private javax.swing.JTextPane jMensajeEnviar;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JScrollPane jScrollPane4;
     private javax.swing.JTable jTablaAmigos;
+    private javax.swing.JLabel jchatear;
     private javax.swing.JLabel saludoCliente;
     private javax.swing.JButton solicitudes;
     // End of variables declaration//GEN-END:variables
